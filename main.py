e4554ac3c5e0e2b14a57584ca385021abd6ebbf1
ferdinand <avikpeferdinard@gmail.com>
2020-09-03 19:17:05 -0500

    initiate commit

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello, SmartNinja!"
+    return render_template("index.html")
+
+@app.route("/about-me")
+def about():
+    return render_template("about.html")
+
 
 
 if __name__ == '__main__':
