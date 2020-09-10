3a0094420622fcf8070ecc7c64b0f8e7ad9c1850
ferdinand <avikpeferdinard@gmail.com>
2020-09-10 18:20:29 -0500

    initiate commit

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    some_text = "Message from the handler."
+    current_year = datetime.datetime.now().year
+
+    return render_template("index.html" , some_text=some_text, current_year=current_year)
 
 @app.route("/about-me")
 def about():
