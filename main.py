8b44d3804862813a07dc682e182de02b90877fb3
ferdinand <avikpeferdinard@gmail.com>
2020-09-10 18:31:34 -0500

    initiate commit

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def index():
     some_text = "Message from the handler."
     current_year = datetime.datetime.now().year
 
-    return render_template("index.html" , some_text=some_text, current_year=current_year)
+    cities = ["Winnipeg", "Brandon", "Steinbach", "Westhawk"]
+    return render_template("index.html" , some_text=some_text, current_year=current_year, cities=cities)
 
 @app.route("/about-me")
 def about():
